d3acce9940ab01f42bb6473e8e4a2f9ac3690454
Luisa Santo <luisasanto@MBP-de-Luisa.home>
2019-01-25 23:44:02 +0000

    clean repository

--- web/inserenumeroprocesso.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <h3>Criar um Processo de Socorro</h3>
-        <form action="inserirprocesso.php" method="post">
-            <p>Numero do Processo: <input type="text" name="numprocessosocorro"/></p>
-            <p><input type="submit" value="Submit"/></p>
-        </form>
-    </body>
-</html>
